ad5833ff4b31f864e874ef68564932fb3bdf3825
Lizaveta Barazna <pavel.khmialinku@standardbank.co.za>
2022-11-07 15:59:14 +0300

    Add primary bean examples

--- src/main/java/spring/svg/di/sfgdi/controllers/MyController.java
+++ src/main/java/spring/svg/di/sfgdi/controllers/MyController.java
@@ -1,13 +1,18 @@
 package spring.svg.di.sfgdi.controllers;
 
 import org.springframework.stereotype.Controller;
+import spring.svg.di.sfgdi.services.GreetingService;
 
 @Controller
 public class MyController {
 
-    public String sayHello() {
-        System.out.println("Hello world");
+    private final GreetingService greetingService;
 
-        return "Hi Liza";
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
     }
 }
