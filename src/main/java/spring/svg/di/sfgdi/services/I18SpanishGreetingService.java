5c9fb2293e8f31455495f8f0d999a4bdac00ac18
Lizaveta Barazna <pavel.khmialinku@standardbank.co.za>
2022-11-07 16:34:53 +0300

    Add spring  default profile example

--- src/main/java/spring/svg/di/sfgdi/services/I18SpanishGreetingService.java
+++ src/main/java/spring/svg/di/sfgdi/services/I18SpanishGreetingService.java
@@ -3,7 +3,7 @@ package spring.svg.di.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18Service")
 public class I18SpanishGreetingService implements GreetingService {
     @Override
